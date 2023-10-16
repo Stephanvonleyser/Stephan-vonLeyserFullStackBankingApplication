78786f86d396b65a66e60dd6793429d61b779d6e
Stephan <vonzeo7@gmail.com>
2023-10-16 01:51:59 -0300

    testing v1

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 //Connect to our DB
 export const connectBD = async () => {
     try {
-		const connection = await mongoose.connect(process.env.MONGO_URI, 
+		const connection = await mongoose.connect(process.env.DB_URI, 
 			{
 				useNewUrlParser: true,
 				useUnifiedTopology:true
